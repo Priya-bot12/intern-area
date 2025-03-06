3f93fdf25b91522fe8dffb874401d7701ff1739b
Priya-bot12 <119518888+Priya-bot12@users.noreply.github.com>
2025-03-06 22:28:49 +0530

    Update ViewAllApplication.jsx

--- client/src/Admin/ViewAllApplication.jsx
+++ client/src/Admin/ViewAllApplication.jsx
@@ -8,7 +8,7 @@ function ViewAllApplication() {
   useEffect(() => {
     const fetchApplication = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/application");
+        const response = await axios.get("https://intern-area.onrender.com/api/application");
         setApplication(response.data);
       } catch (error) {
         alert("Error fetching applications: " + error.message);
