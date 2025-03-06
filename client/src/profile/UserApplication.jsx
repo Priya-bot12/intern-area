f534ad57fdd0cfee516c6fe03ac571deb6a9e24a
Priya-bot12 <119518888+Priya-bot12@users.noreply.github.com>
2025-03-06 23:00:35 +0530

    Update UserApplication.jsx

--- client/src/profile/UserApplication.jsx
+++ client/src/profile/UserApplication.jsx
@@ -15,7 +15,7 @@ function UserApplication() {
   useEffect(() => {
     const fetchApplication = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/application");
+        const response = await axios.get("https://intern-area.onrender.com/api/application");
         setApplication(response.data);
       } catch (error) {
         toast.error("Error fetching applications");
